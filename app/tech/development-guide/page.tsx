b6738cd62b69f9ace48a41467d65224f08adcde1
jakeson-gao <64231110@qq.com>
2025-03-12 13:31:58 +0800

    use client

--- app/tech/development-guide/page.tsx
+++ app/tech/development-guide/page.tsx
@@ -1,1 +1,13 @@
- 
+'use client';
+
+import { NextPage } from 'next';
+
+const DevelopmentGuidePage: NextPage = () => {
+  return (
+    <div>
+      {/* 您的页面内容 */}
+    </div>
+  );
+}
+
+export default DevelopmentGuidePage;
